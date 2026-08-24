435900f11bbd7cafe796cebe97ecc98ef6b26e8d
Zoltan K <12892696+kecsi-san@users.noreply.github.com>
2026-08-24 14:16:47 +0200

    Revert "Major dep upgrades: astro 4->7, nodemailer 6->9 (resolves all 29 flagged vulnerabilities) (#5)"
    This reverts commit 4152c7236c0bc4ae44cf6210aa3e2a9280e9e966.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://nerymed.hu',
   integrations: [
+    tailwind(),
     sitemap(),
   ],
   output: 'static',
